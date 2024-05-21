7715c29ef9bfb7d6b9b256e83f765c5319932ea7
MayDan12 <oluwamayowadan@gmail.com>
2024-05-21 09:48:04 +0000

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,6 +4,8 @@ Use index_range to find the correct indexes to paginate the dataset
 correctly and
 return the appropriate page of the dataset (i.e. the correct list of
 """
+import csv
+from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
@@ -22,27 +24,43 @@ def index_range(page: int, page_size: int) -> tuple:
     return (start_index, end_index)
 
 
-class Pagination:
-    def __init__(self, data: list, page_size: int):
-        """
-        Initialize the pagination class.
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-        Args:
-        - data (list): The list of items to paginate.
-        - page_size (int): The number of items per page.
-        """
-        self.data = data
-        self.page_size = page_size
+    def __init__(self):
+        self.__dataset = None
 
-    def get_page(self, page: int) -> list:
+    def dataset(self) -> List[List]:
+        """Cached dataset
         """
-        Retrieve a single page of items.
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return (self.__dataset)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Retrieve a page from the dataset.
 
         Args:
         - page (int): The page number to retrieve (1-indexed).
+        - page_size (int): The number of items per page.
 
         Returns:
-        - list: The list of items on the requested page.
+        - List[List]: A list of rows corresponding to the page of data.
         """
-        start_index, end_index = index_range(page, self.page_size)
-        return (self.data[start_index:end_index])
+        assert isinstance(page, int) and page > 0, "Page must be int > than 0."
+        assert isinstance(page_size, int) and page_size > 0, "must be int > 0."
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
